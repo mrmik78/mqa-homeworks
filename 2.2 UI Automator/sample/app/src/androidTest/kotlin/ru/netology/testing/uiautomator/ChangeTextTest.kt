04fb203e3d0c36df8b769334475bc708bdd15448
mrmik78 <m.reutsky@mail.ru>
2026-07-02 22:32:31 +0300

    initial commit

--- 2.2 UI Automator/sample/app/src/androidTest/kotlin/ru/netology/testing/uiautomator/ChangeTextTest.kt
+++ 2.2 UI Automator/sample/app/src/androidTest/kotlin/ru/netology/testing/uiautomator/ChangeTextTest.kt
@@ -24,6 +24,11 @@ class ChangeTextTest {
 
     private lateinit var device: UiDevice
     private val textToSet = "Netology"
+    private val textToEmpty = "   "
+    private val textToNewActivity = "Hello World!"
+
+
+
 
 //    @Test
 //    fun testInternetSettings() {
@@ -94,10 +99,9 @@ class ChangeTextTest {
         waitForPackage(SETTINGS_PACKAGE)
 
         device.findObject(
-            UiSelector().resourceId("android:id/title").instance(0)
+            UiSelector().resourceId("android:id/title").instance(3)
         ).click()
     }
-
     @Test
     fun testChangeText() {
         val packageName = MODEL_PACKAGE
@@ -108,9 +112,31 @@ class ChangeTextTest {
 
         val result = device.findObject(By.res(packageName, "textToBeChanged")).text
         assertEquals(result, textToSet)
+        //Thread.sleep(5_000)
     }
+    @Test
+    fun testEmptyInput() {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
 
+        val source = device.findObject(By.res(packageName, "textToBeChanged")).text
+        device.findObject(By.res(packageName, "userInput")).text = textToEmpty
+        device.findObject(By.res(packageName, "buttonChange")).click()
+
+        val result = device.findObject(By.res(packageName, "textToBeChanged")).text
+        assertEquals(result, source)
+        Thread.sleep(5_000)
+    }
+    @Test
+    fun testOpenNewActivity() {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+
+        device.findObject(By.res(packageName, "userInput")).text = textToNewActivity
+        device.findObject(By.res(packageName, "buttonActivity")).click()
+        waitForPackage(packageName)
+
+        val result = device.findObject(By.res(packageName, "text")).text
+        assertEquals(result, textToNewActivity)
+    }
 }
-
-
-
